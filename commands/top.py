87f4da923a47790c7f134b3cfa83c626464ce23c
QuasiStellar <laplas2013@yandex.ru>
2019-12-10 11:40:12 +0300

    top adjustments

--- commands/top.py
+++ commands/top.py
@@ -10,13 +10,12 @@ def top(player_id, command):
     connection = pymysql.connect(host, user, password, user + '$main')
     with connection:
         cur = connection.cursor()
-        cur.execute('SELECT player_id, host_elo, elo FROM players ORDER BY (host_elo + elo) DESC;')
+        cur.execute('SELECT player_id, host_elo, elo FROM players WHERE host_elo + elo > 2000 ORDER BY (host_elo + elo) DESC;')
         rows = cur.fetchmany(min(10, cur.rowcount))
         message = 'ТОП-10:\n\n'
         for row in rows:
             message += message_handler.username(row[0]) + ': ' + str(row[1]) + ' / ' + str(row[2]) + ' ЭЛО\n'
         return [message]
-
 
 top_command = command_system.Command()
 
